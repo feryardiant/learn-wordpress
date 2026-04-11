ae0221bced6b7555c220be88c6277230327be44e
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-04-11 14:55:57 +0700

    Add `blocksy` and its companion plugin as pre-installed theme
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- docker/init-wp.sh
+++ docker/init-wp.sh
@@ -13,6 +13,28 @@ else
     --admin_password="${SITE_ADMIN_PASS}" \
     --admin_email="${SITE_ADMIN_EMAIL}" \
     --skip-email --allow-root
-
-  echo "Site URL: ${SITE_URL}"
 fi
+
+echo "Initializing default Plugins..."
+for plugin in ${SITE_PLUGINS//,/ }; do
+    if wp plugin is-installed "$plugin" --url="${SITE_URL}"; then
+        echo " - $plugin is already installed."
+        continue
+    fi
+
+    wp plugin install "$plugin" --url="${SITE_URL}" --activate
+done
+
+echo "Initializing default Themes..."
+for theme in ${SITE_THEMES//,/ }; do
+    if wp theme is-installed "$theme" --url="${SITE_URL}"; then
+        echo " - $theme is already installed."
+        continue
+    fi
+
+    wp theme install "$plugin" --url="${SITE_URL}"
+done
+
+wp theme activate ${SITE_DEFAULT_THEME} --url="${SITE_URL}"
+
+echo "Site URL: ${SITE_URL}"
