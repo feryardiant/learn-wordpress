43e04f1ae6b611fe0db1068d68232e212fa3f182
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-04-12 20:51:23 +0700

    Move `favicon.icon` to `public` directory
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- docker/init-wp.sh
+++ docker/init-wp.sh
@@ -19,8 +19,9 @@ echo "Initializing default Options..."
 wp option update permalink_structure "/%postname%/"
 wp option update timezone_string "${SITE_TIMEZONE}"
 
-custom_theme_path=$(wp theme path)/${SITE_DEFAULT_THEME}
-cp $custom_theme_path/favicon.ico .
+if [[ ! -f ./favicon.ico ]]; then
+  cp /var/www/public/favicon.ico .
+fi
 
 echo "Initializing default Plugins..."
 for plugin in ${SITE_PLUGINS//,/ }; do
