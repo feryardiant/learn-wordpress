3271bb38fa65acd5b7e26e072154e9cb229464bf
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-04-17 19:12:08 +0700

    Mark item as read when user view it
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- packages/cf7-entry-manager/includes/admin.php
+++ packages/cf7-entry-manager/includes/admin.php
@@ -258,6 +258,8 @@ function admin_management_page(): void {
 	$item = \wpcf7_superglobal_request( 'post', null );
 
 	if ( 'view' === $action && $item = new Item( $item ) ) {
+		$item->mark_read();
+
 		require_once __DIR__ . '/view-entry.php';
 
 		return;
