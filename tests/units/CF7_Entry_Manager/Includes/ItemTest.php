f19ebd463a7b2db1911d0fcfae20331da3071797
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-04-21 06:12:50 +0700

    Init dummy tests
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ tests/units/CF7_Entry_Manager/Includes/ItemTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace UnitTests\CF7_Entry_Manager\Includes;
+
+use Brain\Monkey\Functions;
+use CF7_Entry_Manager\Item;
+use Mockery;
+use UnitTests\CF7_Entry_Manager\TestCase;
+
+/**
+ * Class ItemTest
+ */
+class ItemTest extends TestCase {
+	/**
+	 * Setup before any test in this class runs.
+	 */
+	public static function setUpBeforeClass(): void {
+		parent::setUpBeforeClass();
+
+		require_once static::package_file( 'cf7-entry-manager/includes/class-item.php' );
+	}
+
+	public function test_dummy() {
+		$this->assertTrue( class_exists( Item::class ) );
+	}
+}
---
+++ tests/units/CF7_Entry_Manager/Includes/OptionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace UnitTests\CF7_Entry_Manager\Includes;
+
+use CF7_Entry_Manager\Option;
+use Mockery;
+use UnitTests\CF7_Entry_Manager\TestCase;
+
+/**
+ * Class OptionTest
+ */
+class OptionTest extends TestCase {
+	/**
+	 * Setup before any test in this class runs.
+	 */
+	public static function setUpBeforeClass(): void {
+		parent::setUpBeforeClass();
+
+		require_once static::package_file( 'cf7-entry-manager/includes/class-option.php' );
+	}
+
+	public function test_dummy() {
+		$this->assertTrue( class_exists( Option::class ) );
+	}
+}
