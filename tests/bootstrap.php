bf96ac7f4922b6633e702b959325d5bae0e71c8c
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-04-23 20:21:17 +0700

    Test the `lint-staged`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,22 +1,21 @@
 <?php
+
 /**
  * PHPUnit Bootstrap
  */
 
-$_root = dirname( __DIR__ );
+$_root = dirname(__DIR__);
 
 require_once $_root . '/vendor/autoload.php';
 
 // Mock WordPress constants if needed
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', $_root . '/docker/volumes/wordpress/' );
+if (! defined('ABSPATH')) {
+    define('ABSPATH', $_root . '/docker/volumes/wordpress/');
 }
 
-
-defined( 'WP_TESTS_DOMAIN' ) || define( 'WP_TESTS_DOMAIN', '' );
-defined( 'WP_TESTS_EMAIL' ) || define( 'WP_TESTS_EMAIL', '' );
-defined( 'WP_TESTS_TITLE' ) || define( 'WP_TESTS_TITLE', '' );
-defined( 'WP_PHP_BINARY' ) || define( 'WP_PHP_BINARY', '' );
-
+defined('WP_TESTS_DOMAIN') || define('WP_TESTS_DOMAIN', '');
+defined('WP_TESTS_EMAIL') || define('WP_TESTS_EMAIL', '');
+defined('WP_TESTS_TITLE') || define('WP_TESTS_TITLE', '');
+defined('WP_PHP_BINARY') || define('WP_PHP_BINARY', '');
 
 // Any other test initialization can go here.
